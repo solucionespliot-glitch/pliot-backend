674b551c72b0c7c504ffdfcdaf1618d4acc60c55
Soluciones Pliot <solucionespliot@gmail.com>
2026-03-22 12:14:20 -0300

    arreglo path dotenv

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
+import path from 'path'; 
 
-dotenv.config();
+dotenv.config({path: path.resolve(__dirname '../../.env') });
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
