9a7092a432196396d0ada6c11265e7b8fdcaf8ac
Soluciones Pliot <solucionespliot@gmail.com>
2026-03-22 12:19:13 -0300

    ahora si corregido index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 import path from 'path'; 
 
-dotenv.config({path: path.resolve(__dirname '../../.env') });
+dotenv.config({path: path.resolve(__dirname, '../../.env') });
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
